4928bc270012fd1d059239904e4fe84cfbadb9f6
khalilsafaee <khalil.safaee@gmail.com>
2026-02-22 23:31:37 +0330

    Decision tree model

--- app.py
+++ app.py
@@ -37,5 +37,29 @@ with st.expander('View Data'):
     st.bar_chart(df['class'].value_counts())
 # endregion
 
+# region Train,Test set
+# one-hot encoding
+X = pd.get_dummies(df.drop('class',axis = 1))
+y = df['class']
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2 , random_state=42)
+dt_model = DecisionTreeClassifier(max_depth=5,random_state=42)
+dt_model.fit(X_train,y_train)
+y_pred = dt_model.predict(X_test)
+accuracy = accuracy_score(y_test,y_pred)
+# endregion
+
+st.metric("Model accuracy" ,f"{accuracy:.2%}")
+st.header('Predict for New Car')
+col1 , col2,col3 = st.columns(3)
+with col1:
+    buying = st.selectbox('Buy Price Category',['low','med','high','vhigh'])
+    maint = st.selectbox('maintenance Cost Category',['low','med','high','vhigh'])
+with col2:
+    doors = st.selectbox('Doors Count',['2','3','4','5more'])
+    persons = st.selectbox('Person Capacity',['2','4','more'])
+
+
+
+
 
 
