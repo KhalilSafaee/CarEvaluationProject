8733414599fe12635e48fe00759852b00dfdea4d
khalilsafaee <khalil.safaee@gmail.com>
2026-02-22 22:49:22 +0330

    streamlit section

--- app.py
+++ app.py
@@ -3,23 +3,39 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
-from sklearn import *
+
 
 
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score,classification_report
 
-
-
-# endregiongit branch
-
 # endregion
+# region Function Declare
+# region def Load_data
 
 
+@st.cache_data
 def load_data():
     # columns
     columns = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
     # read files
-    df = pd.read_csv('data/car.data', names=columns, header=None)
+    df = pd.read_csv('data/car.csv', names=columns, header=0 , sep=";")
     return df
+# endregion
+#endregion
+df = load_data()
+# region  Streamlit Page
+st.set_page_config(page_title="Car Evaluation Predictor",layout='wide')
+st.title("Predict Car Evaluation")
+st.markdown('This Program Predict the acceptability of a car based on its features.')
+with st.expander('View Data'):
+    st.dataframe(df.head(10))
+    st.write('Total Row: ',len(df))
+    st.write('Columns: ',df.columns.tolist())
+    st.write('Goal Variable Distribute:')
+    st.bar_chart(df['class'].value_counts())
+# endregion
+
+
+
