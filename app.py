999a8a3b9b3a1c5b37eee4b69e1c0357b900620c
khalilsafaee <khalil.safaee@gmail.com>
2026-02-22 17:11:55 +0330

    add sklearn module

--- app.py
+++ app.py
@@ -2,7 +2,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import *
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score,classification_report
 
 
 # endregion
