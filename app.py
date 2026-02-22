610df7ae4276aa32b1916653a83f273997cf72b8
kimiasafaei81 <kimiasafaei81@gmail.com>
2026-02-22 15:36:58 +0100

    def load_data added

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import *
 
-
 # endregion
 
+def load_data():
+    # columns
+    columns = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
+    # read files
+    df = pd.read_csv('data/car.data', names=columns, header=None)
+    return df
